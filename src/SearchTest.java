08c14278c24612d4ea91f738f7dfe11520e4cd93
Gustav Sandström <gussan@omegapoint.se>
2019-03-07 17:04:25 +0100

    added tests for rank function, validated no, words, half the words, all the words

--- src/SearchTest.java
+++ src/SearchTest.java
@@ -1,39 +1,52 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class SearchTest
+class SearchTest
 {
 
     @Test
-    public void main()
+    void main()
     {
     }
 
     @Test
-    public void rank()
+    void rankIfAllWordsMatchIsOne()
     {
+        assertEquals(1.0, Search.rank("a", new String[]{"a"}));
     }
 
     @Test
-    public void doubleToProccentStringOf0()
+    void rankIfNoWordsMatchIsZero()
+    {
+        assertEquals(0.0, Search.rank("a", new String[]{"b"}));
+    }
+
+    @Test
+    void rankIfHalfWordsMatchIsZero()
+    {
+        assertEquals(0.5, Search.rank("a", new String[]{"b", "a"}));
+    }
+
+    @Test
+    void doubleToProccentStringOf0()
     {
         assertEquals("0%", Search.doubleToProccentString(0.0));
     }
 
     @Test
-    public void doubleToProccentStringOf1()
+    void doubleToProccentStringOf1()
     {
         assertEquals("100%", Search.doubleToProccentString(1.0));
     }
 
     @Test
-    public void doubleToProccentStringOf2()
+    void doubleToProccentStringOf2()
     {
         assertEquals("200%", Search.doubleToProccentString(2.0));
     }
 
     @Test
-    public void doubleToProccentStringOfNegative1()
+    void doubleToProccentStringOfNegative1()
     {
         assertEquals("-100%", Search.doubleToProccentString(-1.0));
     }
