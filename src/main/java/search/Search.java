b446c0beee3a5a148c7c31e4c4fe783fe9950681
Gustav Sandström <gussan@omegapoint.se>
2019-03-08 22:25:08 +0100

    minor change, functionallity completed

--- src/main/java/search/Search.java
+++ src/main/java/search/Search.java
@@ -6,7 +6,6 @@ import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
